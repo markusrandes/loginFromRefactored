b7c3b78abd5cc502b6b76f7ea8e868e216caa600
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:21:55 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string Login;
+            string Password;
+            Console.WriteLine("Login@: ");
+            Login = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            Password = Console.ReadLine();
+
+            if ((Login == "admin") && (Password == "pass1234"))
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else if ((Login == "admin") && (Password != "pass1234"))
+            {
+                Console.WriteLine("The password is incorrect");
+            }
+            else if ((Login != "admin") && (Password == "pass1234"))
+            {
+                Console.WriteLine("The login is incorrect.");
+            }
+            else
+            {
+                Console.WriteLine("You shall not pass!");
+            }
+
+        }
+    }
+}
